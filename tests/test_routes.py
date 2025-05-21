2e813c205029acb1fb462fa90cc3325d55d2c199
diegocastanibm <diego.castan@ibm.com>
2025-05-21 16:05:19 -0400

    Add extra test method not allowed

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -173,3 +173,8 @@ class TestAccountService(TestCase):
         )
         data = response.get_json()
         self.assertEqual(len(data), NUMBER_OF_ACCOUNTS)
+    
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
