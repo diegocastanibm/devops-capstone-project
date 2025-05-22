886edc5ecb715e49eb77942b971b6838a1c4d4a5
diegocastanibm <diego.castan@ibm.com>
2025-05-22 14:12:29 -0400

    Added badge for GitHub Actions

--- service/routes.py
+++ service/routes.py
@@ -61,7 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
-# ... place you code here to LIST accounts ...
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -78,7 +78,7 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_account(account_id):
     """
@@ -93,15 +93,11 @@ def read_account(account_id):
             f"Account with account ID {account_id} does not exist",
         )
     return account.serialize(), status.HTTP_200_OK
-        
-
-
-
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+
 @app.route("/accounts/<account_id>", methods=["POST"])
 def update_account(account_id):
     """Update an existing Account"""
@@ -120,7 +116,7 @@ def update_account(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_account(account_id):
     """
